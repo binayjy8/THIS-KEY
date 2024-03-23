d301f665c16c8c0a8f4ca892d574abff2d231108
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-23 16:44:17 +0530

    code

---
+++ app.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "Binay",
+    age: 25,
+    eng: 45,
+    math: 34,
+    phy: 45,
+    getAvg() {
+        let avg = (this.eng + this.math + this.phy) / 3;
+        console.log(avg);
+    }
+}
